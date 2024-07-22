5fb9c75ea24c11b39b7459d63922246061e8da3c
Fahad <fahadmasood03@gmail.com>
2024-07-23 01:40:52 +0530

    Add Accordion full code

--- src/Accordion/AccordionItems/index.jsx
+++ src/Accordion/AccordionItems/index.jsx
@@ -1,13 +1,35 @@
 import React, { useState } from "react";
 
-export const AccordionItems = ({ qna }) => {
+export const AccordionItems = ({ qna, expanded, currentId, setCurrentId }) => {
   const [show, setShow] = useState(false);
+
+  const setToggle = (id) => {
+    if (id === undefined || id === null || id === currentId) {
+      setShow(!show);
+    } else {
+      setCurrentId(id);
+      setShow(true);
+    }
+  };
+
   return (
-    <div className="accordion-items" onClick={() => setShow(!show)}>
+    <div
+      className="accordion-items"
+      onClick={() => setToggle(expanded ? qna.id : undefined)}
+    >
       <h3 className="title">
-        {qna.question} <span className="btn">{show ? "-" : "+"}</span>
+        {qna.question}{" "}
+        <span className="btn">
+          {show && (currentId === qna.id || !expanded) ? "-" : "+"}
+        </span>
       </h3>
-      {show && <p className="content">{qna.answer}</p>}
+      {show && (
+        <>
+          {((expanded && currentId === qna.id) || !expanded) && (
+            <p className="content">{qna.answer}</p>
+          )}
+        </>
+      )}
     </div>
   );
 };
--- src/Accordion/index.jsx
+++ src/Accordion/index.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 import data from "./data.json";
 import { AccordionItems } from "./AccordionItems";
 
 const Accordion = () => {
+  const [currentId, setCurrentId] = useState("");
   return (
     <div className="accordion">
       <h2>ACCORDION</h2>
       {data.jokes.map((joke, i) => {
-        return <AccordionItems qna={joke} key={joke.id} />;
+        return (
+          <AccordionItems
+            qna={joke}
+            key={joke.id}
+            expanded={true}
+            currentId={currentId}
+            setCurrentId={setCurrentId}
+          />
+        );
       })}
     </div>
   );
